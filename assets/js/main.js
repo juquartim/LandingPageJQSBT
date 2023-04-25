5e9773872462a71491af30a98f18fa5e708d07d6
Julia Quartim <70655923+juquartim@users.noreply.github.com>
2023-04-25 14:43:12 +0100

    Button adjusts

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,27 +103,5 @@ document.addEventListener('DOMContentLoaded', () => {
         }));
     }
 
-    /**
-   * Initiate glightbox
- 
-  const glightbox = GLightbox({
-    selector: '.glightbox'
-  });  */
-
-
-    /**
-     * Animation on scroll function and init
-     */
-    function aos_init() {
-        AOS.init({
-            duration: 1000,
-            easing: 'ease-in-out',
-            once: true,
-            mirror: false
-        });
-    }
-    window.addEventListener('load', () => {
-        aos_init();
-    });
 
 });
